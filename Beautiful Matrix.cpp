72784d17d217e7f8334e4c8d229d85222664c8a7
pronoyc11 <pronoyc11@gmail.com>
2026-04-08 11:38:34 +0600

    Module 2 — Lecture 8 complete

---
+++ Beautiful Matrix.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main() {
+
+    int p,q;
+
+    for(int i= 1; i<=5; i++) {
+        for(int j=1; j<=5; j++) {
+            int num;
+            cin>>num;
+
+            if(num == 1) {
+                p= i;
+                q= j;
+            }
+        }
+    }
+ int moves = abs(3-p) + abs(3-q) ;
+
+ cout<< moves<<endl;
+    return 0;
+}
