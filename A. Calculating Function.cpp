f482a301f0a129f640633084ed610c1d426a1c2c
pronoyc11 <pronoyc11@gmail.com>
2026-04-13 20:24:28 +0600

    DONE MODULE 3 – LECTURE 3

---
+++ A. Calculating Function.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+
+    long long n;
+
+    cin>>n;
+    long long sum ;
+    if(n%2 !=0 ){
+     sum = (n/2) - n;
+    }else{
+        sum = n/2;
+    }
+
+    cout<<sum<<endl;
+
+
+return 0;
+}
